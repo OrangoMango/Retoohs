cb3af502817d7a1335d92b0249f94a53913f89fb
OrangoMango <orapama06@gmail.com>
2023-07-09 16:56:41 +0200

    Minor changes

--- src/main/java/com/orangomango/retoohs/ui/GameOverScreen.java
+++ src/main/java/com/orangomango/retoohs/ui/GameOverScreen.java
@@ -5,14 +5,22 @@ import javafx.scene.canvas.*;
 import javafx.scene.paint.Color;
 import javafx.animation.*;
 import javafx.util.Duration;
+import javafx.scene.text.TextAlignment;
+import javafx.scene.input.MouseButton;
 import javafx.scene.media.MediaPlayer;
 
 import com.orangomango.retoohs.MainApplication;
 
 public class GameOverScreen{
 	private MediaPlayer mediaPlayer;
+	private long time;
+	private int score;
+	private int bossesKilled;
 	
-	public GameOverScreen(){
+	public GameOverScreen(long time, int s, int b){
+		this.time = time;
+		this.score = s;
+		this.bossesKilled = b;
 		MainApplication.schedule(() -> this.mediaPlayer = MainApplication.playSound(MainApplication.GAMEOVER_BACKGROUND_MUSIC, true), 500);
 	}
 
@@ -22,9 +30,11 @@ public class GameOverScreen{
 		GraphicsContext gc = canvas.getGraphicsContext2D();
 		
 		canvas.setOnMousePressed(e -> {
-			if (this.mediaPlayer != null) this.mediaPlayer.stop();
-			HomeScreen hs = new HomeScreen();
-			MainApplication.stage.getScene().setRoot(hs.getLayout());
+			if (e.getButton() == MouseButton.PRIMARY){
+				if (this.mediaPlayer != null) this.mediaPlayer.stop();
+				HomeScreen hs = new HomeScreen();
+				MainApplication.stage.getScene().setRoot(hs.getLayout());
+			}
 		});
 		
 		Timeline loop = new Timeline(new KeyFrame(Duration.millis(1000.0/MainApplication.FPS), e -> update(gc)));
@@ -36,6 +46,12 @@ public class GameOverScreen{
 	}
 	
 	private void update(GraphicsContext gc){
-		
+		gc.clearRect(0, 0, MainApplication.WIDTH, MainApplication.HEIGHT);
+		gc.setFill(Color.ORANGE);
+		gc.fillRect(0, 0, MainApplication.WIDTH, MainApplication.HEIGHT);
+		gc.setFill(Color.BLACK);
+		gc.setFont(GameScreen.FONT_45);
+		gc.setTextAlign(TextAlignment.CENTER);
+		gc.fillText("GAME OVER\n\nYou survived "+(this.time/1000)+" seconds\nwith a score of "+this.score+"\nand killed "+this.bossesKilled+" bosses\n\nClick the screen to exit", MainApplication.WIDTH/2, 125);
 	}
 }
